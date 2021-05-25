e5f9f21ac989e30ad59da7f09a461068f2b14e28
hypoalien <hypoalien@gmail.com>
2021-05-26 04:32:08 +0530

    added favicon

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -12,9 +12,9 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'Blowouts | Tickets',
+  description: 'We sell exclusive event tickets for best price',
+  keywords: 'events,parties,goa,darkpsy,hitech',
 }
 
 export default Meta
